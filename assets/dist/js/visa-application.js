a61946d7fd0db06368f0cf1d827b3daaff3bc9a2
Viktoriia <toryvna1993@gmail.com>
2019-08-06 18:07:26 +0300

    change visa-app page

--- assets/dist/js/visa-application.js
+++ assets/dist/js/visa-application.js
@@ -1,26 +1,20 @@
 $(document).ready(function(){
-    
-    var custSelect = $('select');
-  
-    // Options for custom Select
-    jcf.setOptions('Select', {
-        wrapNative: false,
-        wrapNativeOnMobile: false,
-        fakeDropInBody: false,
-        maxVisibleItems: 10
-    });
-    
-    jcf.replace(custSelect);
+
 
     //add applicant
     var max_fields = 3;
     var num = 1;
+    var content = $('.main-clone').clone().removeClass('main-clone');
+    // console.log(content.html());
     $('.applicants-wrap').each(function () {
+        
         var $wrapper = $('.multi-fields', this);
         $("#addApplicant", $(this)).click(function (e) {
             if (num < max_fields) {
                 num++;
-                $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper).find('.numApplicant').text(num);
+                $(content, $wrapper).clone(true).appendTo($wrapper).addClass('clone' + num).find('.numApplicant').text(num);
+                // $wrapper.addClass('class');
+                 jcf.replace($('.clone' + num).find('select'));
                 if(num == max_fields){
                     $('#addApplicant').hide();
                 }
@@ -29,12 +23,30 @@ $(document).ready(function(){
     });
 
     //add place
+    var el = 1;
+    var con = $('.main-place').clone().removeClass('main-place');
+
     $('.places-wrap').each(function () {
         var $wrapper = $('.multi-places', this);
         $(".addPlace", $(this)).click(function (e) {
-            $('.multi-place:first-child', $wrapper).clone(true).appendTo($wrapper);
+            el++;
+            $(con, $wrapper).clone(true).appendTo($wrapper).addClass('element' + el);
+            // $wrapper.addClass('class');
+             jcf.replace($('.element' + el).find('select'));
+
+            // $('.multi-place:first-child', $wrapper).clone(true).appendTo($wrapper);
         });
     });
+
+    // Options for custom Select
+    jcf.setOptions('Select', {
+        wrapNative: false,
+        wrapNativeOnMobile: false,
+        fakeDropInBody: false,
+        maxVisibleItems: 10
+    });
+    
+    jcf.replace($('.main-clone select, .main-place select, .currentSelect'));
 
     //datepicker
     $( ".datepicker" ).datepicker({
@@ -46,21 +58,30 @@ $(document).ready(function(){
 
     $( ".datepicker-year" ).datepicker({
         changeYear: true,
-        showButtonPanel: true,
         dateFormat: 'yy',
+        yearRange: "-100:+0",
         onClose: function(dateText, inst) { 
             var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
             $(this).datepicker('setDate', new Date(year, 1));
         }
-    });
-    $(".datepicker-year").focus(function () {
-        $(".ui-datepicker-calendar").hide();
     });
 
     //show/hide accordion
     $('.accordion-heading').on('click', function(){
         $(this).parent().toggleClass('active');
     });
+
+    //show all applicants
+    $('.applicants-row .show-all').on('click', function(){
+        $(this).hide();
+        $(this).closest('.applicants-row').addClass('show');
+    });
+
+    //переход между шагами
+    $('.form-buttons button').on('click', function(){
+        $('.step').removeClass('active');
+        $('.' + $(this).attr('data-step')).addClass('active');
+    })
 
 
 });
